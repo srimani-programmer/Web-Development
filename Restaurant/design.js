1ad0cf1fc4a39235e87701b0a2074c2330efd668
srimani-programmer <srimani.scholar@gmail.com>
2018-08-30 13:29:13 +0530

    Final Commit

---
+++ Restaurant/design.js
@@ -0,0 +1,21 @@
+function openNav() {
+  		  document.getElementById("mySidenav").style.width = "410px";
+		}
+
+		function closeNav() {
+  		  document.getElementById("mySidenav").style.width = "0";
+		}
+		function opened() {
+  		  document.getElementById("profile").style.width = "250px";
+		}
+
+		function closed() {
+  		  document.getElementById("profile").style.width = "0";
+		}
+		function opening() {
+  		  document.getElementById("shared").style.width = "350px";
+		}
+
+		function closing() {
+  		  document.getElementById("shared").style.width = "0";
+		}
